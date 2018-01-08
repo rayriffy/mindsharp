97af210b15032446a02bb1c96d26f65b59fef66f
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-01-08 13:01:51 +0700

    Polymer that s---

--- cmd/donate_thx.php
+++
@@ -1,3 +0,0 @@
-<?
-  header('Location: donation/thanks.php');
-?>
--- js/companion.js
+++
@@ -1,1 +0,0 @@
-(function(){'use strict';var workerScript=document.currentScript&&document.currentScript.dataset.serviceWorker;if(workerScript&&'serviceWorker'in navigator){navigator.serviceWorker.register(workerScript);}})();
